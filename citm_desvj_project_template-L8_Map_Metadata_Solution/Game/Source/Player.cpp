574f610e8587ad9ef64a2043eb6162bc6d106062
joanmarquesbesses <85459149+joanmarquesbesses@users.noreply.github.com>
2023-11-05 23:03:08 +0100

    finish

---
+++ citm_desvj_project_template-L8_Map_Metadata_Solution/Game/Source/Player.cpp
@@ -0,0 +1,322 @@
+#include "Player.h"
+#include "App.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Render.h"
+#include "Scene.h"
+#include "Log.h"
+#include "Point.h"
+#include "Physics.h"
+#include "Window.h"
+
+Player::Player() : Entity(EntityType::PLAYER)
+{
+	name.Create("Player");
+}
+
+Player::~Player() {
+
+}
+
+bool Player::Awake() {
+
+	//L03: DONE 2: Initialize Player parameters
+	position = iPoint(config.attribute("x").as_int(), config.attribute("y").as_int());
+	initPosition = position;
+	// idle animation - just one sprite
+	idleAnim.PushBack({ 0, 0, 24, 32 });
+	idleAnim.PushBack({ 27, 0, 25, 32 });
+	idleAnim.PushBack({ 55, 0, 25, 32 });
+	idleAnim.PushBack({ 27, 0, 25, 32 });
+	idleAnim.loop = true;
+	idleAnim.speed = 0.1f;
+
+	walkAnim.PushBack({ 0, 37, 26, 29 });
+	walkAnim.PushBack({ 29, 37, 27, 29 });
+	walkAnim.PushBack({ 59, 37, 27, 29 });
+	walkAnim.loop = true;
+	walkAnim.speed = 0.1f;
+
+	dieAnim.PushBack({ 27, 73, 25, 39 });
+	dieAnim.PushBack({ 55, 81, 23, 31 });
+	dieAnim.PushBack({ 85, 93, 41, 21 });
+	dieAnim.loop = false;
+	dieAnim.speed = 0.05f;
+
+	jumpAnim1.PushBack({ 0, 115, 26, 31 });
+	jumpAnim1.loop = false;
+
+	jumpAnim2.PushBack({ 57, 115, 25, 31 });
+	jumpAnim2.PushBack({ 85, 115, 25, 31 });
+	jumpAnim2.loop = false;
+	jumpAnim2.speed = 0.2f;
+
+	return true;
+}
+
+bool Player::Start() {
+
+	texture = app->tex->Load(config.attribute("texturePath").as_string());
+
+	state = IDLE;
+	// L07 DONE 5: Add physics to the player - initialize physics body
+	app->tex->GetSize(texture, texW, texH);
+	currentAnimation = &dieAnim;
+	pbody = app->physics->CreateCircle(position.x, position.y, currentAnimation->GetCurrentFrame().w / 2, bodyType::DYNAMIC);
+
+	// L07 DONE 6: Assign player class (using "this") to the listener of the pbody. This makes the Physics module to call the OnCollision method
+	pbody->listener = this;
+
+	// L07 DONE 7: Assign collider type
+	pbody->ctype = ColliderType::PLAYER;
+
+	//initialize audio effect
+	pickCoinFxId = app->audio->LoadFx(config.attribute("coinfxpath").as_string());
+	victory = app->audio->LoadFx(config.attribute("winfxpath").as_string());
+	app->audio->PlayMusic(config.attribute("musicpath").as_string());
+	death = false;
+	flip = false;
+
+	godmode = false;
+
+	return true;
+}
+
+bool Player::Update(float dt)
+{
+	// L07 DONE 5: Add physics to the player - updated player position using physics
+
+	//L03: DONE 4: render the player texture and modify the position of the player using WSAD keys and render the texture
+	b2Vec2 velocity = b2Vec2(0, -GRAVITY_Y);
+
+	if (app->input->GetKey(SDL_SCANCODE_F10) == KEY_DOWN) {
+		if (!godmode) {
+			pbody->body->SetGravityScale(0.0f);
+			godmode = true;
+		}
+		else {
+			pbody->body->SetGravityScale(1.0f);
+			godmode = false;
+		}
+	}
+
+	if (godmode) {
+		if (app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) {
+			velocity.y = -0.5 * dt;
+		}
+	}
+
+	switch (state)
+	{
+	case IDLE:
+		currentAnimation = &idleAnim;
+		break;
+	case WALIKING:
+		currentAnimation = &walkAnim;
+		break;
+	case DIE:
+		currentAnimation = &dieAnim;
+		break;
+	case JUMP1:
+		currentAnimation = &jumpAnim1;
+		break;
+	case JUMP2:
+		currentAnimation = &jumpAnim2;
+		break;
+	default:
+		break;
+	}
+
+	if (app->input->GetKey(SDL_SCANCODE_F3) == KEY_REPEAT) {
+		respawn();
+	}
+
+	if (death) {
+		state = DIE;
+		if (dieAnim.HasFinished()) {
+			dieAnim.Reset();
+			state = IDLE;
+			respawn();
+			death = false;
+		}
+	}
+
+	if (!death) {
+
+		b2ContactEdge* contact = pbody->body->GetContactList();
+		if (contact != nullptr) {
+			b2Vec2 contactPonts = contact->contact->GetManifold()->localNormal;
+			if (contactPonts.y == -1) {
+				if (state == JUMP2) {
+					jumpAnim2.Reset();
+					state = IDLE;
+				}
+				jumps = 2;
+			}
+		}
+
+		if (app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+			velocity.x = -0.5 * dt;
+			flip = true;
+			if (state == IDLE) {
+				state = WALIKING;
+			}
+			if (contact != nullptr) {
+				b2Vec2 contactPonts = contact->contact->GetManifold()->localNormal;
+				if (contactPonts.y != -1) {
+					state = JUMP2;
+				}
+			}
+		}
+
+		if (app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+			velocity.x = 0.5 * dt;
+			flip = false;
+			if (state == IDLE) {
+				state = WALIKING;
+			}
+			if (contact != nullptr) {
+				b2Vec2 contactPonts = contact->contact->GetManifold()->localNormal;
+				if (contactPonts.y != -1) {
+					state = JUMP2;
+				}
+			}
+		}
+
+		if (app->input->GetKey(SDL_SCANCODE_D) == KEY_IDLE && app->input->GetKey(SDL_SCANCODE_A) == KEY_IDLE) {
+			if (state == WALIKING) {
+				state = IDLE;
+			}
+		}
+
+		if (app->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN) {
+			if (jumps > 0) {
+				state = JUMP1;
+				if (contact != nullptr) {
+					b2Vec2 contactPonts = contact->contact->GetManifold()->localNormal;
+					if (contactPonts.y == 0 && (contactPonts.x == 1 || contactPonts.x == -1)) {
+						jumps = 0;
+						remainingJumpSteps = maxremainingJumpSteps;
+						jumpForceReduce = 0;
+					}
+					else {
+						jumps--;
+						remainingJumpSteps = maxremainingJumpSteps;
+						jumpForceReduce = 0;
+					}
+				}
+				else {
+					jumps = 0;
+					remainingJumpSteps = maxremainingJumpSteps;
+					jumpForceReduce = 0;
+				}
+			}
+		}
+
+		if (remainingJumpSteps > 0) {
+			float force = pbody->body->GetMass() * 10 / (1 / 30.0);
+			force /= 2.7f;
+			force -= jumpForceReduce;
+			jumpForceReduce = maxremainingJumpSteps - remainingJumpSteps;
+			pbody->body->ApplyForceToCenter(b2Vec2(0, -(force * dt)), true);
+			remainingJumpSteps--;
+			if (jumpForceReduce > force) {
+				remainingJumpSteps = 0;
+				jumpForceReduce = 0;
+			}
+			if (contact != nullptr) {
+				b2Vec2 contactPonts = contact->contact->GetManifold()->localNormal;
+				if (contactPonts.y == 1) {
+					force = 0;
+					remainingJumpSteps = 0;
+					jumpForceReduce = 0;
+				}
+			}
+		}
+
+		if (remainingJumpSteps <= 0) {
+			if (state == JUMP1) {
+				jumpAnim1.Reset();
+				state = JUMP2;
+			}
+		}
+	}
+
+	pbody->body->SetLinearVelocity(velocity);
+	b2Transform pbodyPos = pbody->body->GetTransform();
+	position.x = METERS_TO_PIXELS(pbodyPos.p.x) - currentAnimation->GetCurrentFrame().w / 2;
+	position.y = METERS_TO_PIXELS(pbodyPos.p.y) - currentAnimation->GetCurrentFrame().h / 2;
+
+	if (flip) {
+		app->render->DrawTexturePR(texture, position.x, position.y, &currentAnimation->GetCurrentFrame());
+	}else{
+		app->render->DrawTexture(texture, position.x, position.y, &currentAnimation->GetCurrentFrame());
+	}
+
+	if (app->input->GetKey(SDL_SCANCODE_F3) == KEY_DOWN) {
+		pbody->body->SetActive(false);
+		position = initPosition;
+		b2Vec2 pbinitPosition = b2Vec2(initPosition.x, initPosition.y);
+		pbody->body->SetTransform(pbinitPosition, 0);
+		pbody->body->SetActive(true);
+	}
+
+	currentAnimation->Update();
+
+	return true;
+}
+
+bool Player::CleanUp()
+{
+	return true;
+}
+
+// L07 DONE 6: Define OnCollision function for the player. 
+void Player::OnCollision(PhysBody* physA, PhysBody* physB) {
+
+	switch (physB->ctype)
+	{
+	case ColliderType::PLATFORM:
+		LOG("Collision PLATFORM");
+		break;
+	case ColliderType::ITEM:
+		if (physB->ctype == ColliderType::ITEM) {
+			app->entityManager->DestroyEntity(physB->listener);
+		}
+		LOG("Collision ITEM");
+		app->audio->PlayFx(pickCoinFxId);
+		break;
+	case ColliderType::VICTORY:
+		LOG("Collision UNKNOWN");
+		app->audio->PlayFx(victory);
+		break;
+	case ColliderType::DEATH:
+		if (!godmode) {
+			death = true;
+		}
+		LOG("Collision UNKNOWN");
+		break;
+	case ColliderType::UNKNOWN:
+		LOG("Collision UNKNOWN");
+		break;
+	default:
+		break;
+	}
+}
+
+void Player::respawn()
+{
+	b2Vec2 initPos = b2Vec2(PIXEL_TO_METERS(initPosition.x), PIXEL_TO_METERS(initPosition.y));
+	pbody->body->SetTransform(initPos, 0);
+	position = initPosition;
+	app->render->camera.y = ((position.y - currentAnimation->GetCurrentFrame().h / 2) - (app->scene->windowH / 2)) * -1;
+	app->render->camera.x = ((position.x - currentAnimation->GetCurrentFrame().w / 2) - (app->scene->windowW / 2)) * -1;
+
+	if (app->render->camera.x >= 0) {
+		app->render->camera.x = 0;
+	}
+	if (app->render->camera.y >= 0) {
+		app->render->camera.y = 0;
+	}
+}
---
+++ citm_desvj_project_template-L8_Map_Metadata_Solution/Game/Source/Scene.cpp
@@ -0,0 +1,144 @@
+#include "App.h"
+#include "Input.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Render.h"
+#include "Window.h"
+#include "Scene.h"
+#include "Map.h"
+#include "Item.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+#define PIXELS_PER_METER 50.0f // if touched change METER_PER_PIXEL too
+#define METER_PER_PIXEL 0.02f // this is 1 / PIXELS_PER_METER !
+
+#define METERS_TO_PIXELS(m) ((int) floor(PIXELS_PER_METER * m))
+#define PIXEL_TO_METERS(p)  ((float) METER_PER_PIXEL * p)
+
+Scene::Scene() : Module()
+{
+	name.Create("scene");
+}
+
+// Destructor
+Scene::~Scene()
+{}
+
+// Called before render is available
+bool Scene::Awake(pugi::xml_node config)
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	//L03: DONE 3b: Instantiate the player using the entity manager
+	//L04 DONE 7: Get player paremeters
+	player = (Player*) app->entityManager->CreateEntity(EntityType::PLAYER);
+	//Assigns the XML node to a member in player
+	player->config = config.child("player");
+
+	//Get the map name from the config file and assigns the value in the module
+	app->map->name = config.child("map").attribute("name").as_string();
+	app->map->path = config.child("map").attribute("path").as_string();
+
+	// iterate all items in the scene
+	// Check https://pugixml.org/docs/quickstart.html#access
+	for (pugi::xml_node itemNode = config.child("item"); itemNode; itemNode = itemNode.next_sibling("item"))
+	{
+		Item* item = (Item*)app->entityManager->CreateEntity(EntityType::ITEM);
+		item->parameters = itemNode;
+	}
+
+	return ret;
+}
+
+// Called before the first frame
+bool Scene::Start()
+{
+	// NOTE: We have to avoid the use of paths in the code, we will move it later to a config file
+	//img = app->tex->Load("Assets/Textures/test.png");
+	
+	//Music is commented so that you can add your own music
+	//app->audio->PlayMusic("Assets/Audio/Music/music_spy.ogg");
+
+	//Get the size of the window
+	app->win->GetWindowSize(windowW, windowH);
+
+	//Get the size of the texture
+	//app->tex->GetSize(img, texW, texH);
+
+	textPosX = (float)windowW / 2 - (float)texW / 2;
+	textPosY = (float)windowH / 2 - (float)texH / 2;
+
+	app->render->camera.y = ((player->position.y - player->texH/2) - windowH / 2) * -1;
+	app->render->camera.x = ((player->position.x- player->texW/2) - (windowW / 2)) * -1;
+
+	if (app->render->camera.x >= 0) {
+		app->render->camera.x = 0;
+	}
+	if (app->render->camera.y >= 0) {
+		app->render->camera.y = 0;
+	}
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::Update(float dt)
+{
+	//L02 DONE 3: Make the camera movement independent of framerate
+	float camSpeed = 1; 
+	int limitCamX = player->position.x - (windowW / 2);
+	if (limitCamX > 0 && limitCamX < (app->map->getMapWidth() - windowW)) {
+		app->render->camera.x = (player->position.x - windowW / 2) * -1;
+	}
+
+	int limitCamY = player->position.y - (windowH / 2);
+	if (limitCamY > 0 && limitCamY < (app->map->getMapHeght() - windowH)) {
+		app->render->camera.y = (player->position.y - windowH / 2) * -1;
+	}
+	
+
+	if(app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		app->render->camera.y -= (int)ceil(camSpeed * dt);
+
+	if(app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		app->render->camera.y += (int)ceil(camSpeed * dt);
+
+	if(app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		app->render->camera.x -= (int)ceil(camSpeed * dt);
+
+	if(app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		app->render->camera.x += (int)ceil(camSpeed * dt);
+
+	// Renders the image in the center of the screen 
+	//app->render->DrawTexture(img, (int)textPosX, (int)textPosY);
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PostUpdate()
+{
+	bool ret = true;
+
+	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+// Called before quitting
+bool Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	return true;
+}

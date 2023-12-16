4600a54405d18e5f49c263a83527a15bac13e339
Jusstox <justot1703@gmail.com>
2023-12-17 00:38:33 +0100

    Anims slime

--- citm_desvj_project_template-L8_Map_Metadata_Solution/Game/Source/EnemySlime.cpp
+++ citm_desvj_project_template-L8_Map_Metadata_Solution/Game/Source/EnemySlime.cpp
@@ -7,32 +7,37 @@ EnemySlime::EnemySlime() :Enemy()
 
 bool EnemySlime::Awake()
 {
-	flyinganim.PushBack({ 0, 0, 39, 35 });
-	flyinganim.PushBack({ 40, 0, 38, 34 });
-	flyinganim.PushBack({ 83, 0, 38, 33 });
-	flyinganim.PushBack({ 122, 0, 39, 32 });
-	flyinganim.PushBack({ 161, 0, 39, 35 });
-	flyinganim.PushBack({ 0, 46, 42, 33 });
+	flyinganim.PushBack({ 0, 0, 16, 14 });
+	flyinganim.PushBack({ 23, 0, 17, 14 });
+	flyinganim.PushBack({ 45, 0, 17, 14 });
+	flyinganim.PushBack({ 23, 0, 17, 14 });
 	flyinganim.loop = true;
-	flyinganim.speed = 0.15;
+	flyinganim.speed = 0.1;
 
-	flyinganimchase.PushBack({ 0, 127, 39, 35 });
-	flyinganimchase.PushBack({ 40, 127, 38, 34 });
-	flyinganimchase.PushBack({ 83, 127, 38, 33 });
-	flyinganimchase.PushBack({ 122, 127, 39, 32 });
-	flyinganimchase.PushBack({ 161, 127, 39, 35 });
-	flyinganimchase.PushBack({ 0, 173, 42, 33 });
+	flyinganimchase.PushBack({ 0, 34, 17, 15 });
+	flyinganimchase.PushBack({ 22, 34, 17, 15 });
+	flyinganimchase.PushBack({ 44, 34, 19, 15 });
+	flyinganimchase.PushBack({ 68, 34, 18, 16 });
+	flyinganimchase.PushBack({ 44, 34, 19, 15 });
+	flyinganimchase.PushBack({ 91, 34, 17, 15 });
+	flyinganimchase.PushBack({ 113, 34, 17, 15 });
 	flyinganimchase.loop = true;
-	flyinganimchase.speed = 0.15;
+	flyinganimchase.speed = 0.05;
 
-	dieanim.PushBack({ 0, 215, 43, 24 });
-	dieanim.PushBack({ 47, 92, 40, 21 });
-	dieanim.PushBack({ 98, 91, 28, 21 });
-	dieanim.PushBack({ 138, 100, 23, 8 });
-	dieanim.PushBack({ 171, 101, 6, 5 });
-	dieanim.PushBack({ 188, 101, 6, 3 });
-	dieanim.loop = false;
-	dieanim.speed = 0.1;
+	dieanim.PushBack({ 0, 99, 17, 15 });
+	dieanim.PushBack({ 22, 99, 19, 15 });
+	dieanim.PushBack({ 46, 99, 22, 15 });
+	dieanim.PushBack({ 73, 99, 26, 15 });
+	dieanim.PushBack({ 104, 99, 28, 15 });
+	dieanim.PushBack({ 137, 99, 30, 15 });
+	dieanim.PushBack({ 171, 99, 32, 15 });
+	dieanim.PushBack({ 208, 99, 25, 15 });
+	dieanim.PushBack({ 238, 99, 23, 15 });
+	dieanim.PushBack({ 266, 99, 23, 15 });
+	dieanim.PushBack({ 294, 100, 9, 15 });
+	dieanim.PushBack({ 0, 0, 0, 0 });
+	dieanim.loop = true;
+	dieanim.speed = 0.0918;
 
 	return true;
 }
@@ -40,13 +45,13 @@ bool EnemySlime::Awake()
 bool EnemySlime::Start()
 {
 	position = iPoint(250, 350);
-	velocity = b2Vec2(0, 0);
+	velocity = b2Vec2(0, +10);
 	initPosition = position;
 	texture = app->tex->Load("Assets/Textures/Slime.png");
-	currentAnimation = &flyinganim;
+	currentAnimation = &flyinganimchase;
 
 	// L07 DONE 5: Add physics to the player - initialize physics body
-	pbody = app->physics->CreateCircle(position.x, position.y, currentAnimation->GetCurrentFrame().w / 3, bodyType::DYNAMIC);
+	pbody = app->physics->CreateCircle(position.x, position.y, currentAnimation->GetCurrentFrame().w / 2, bodyType::DYNAMIC);
 
 	// L07 DONE 6: Assign player class (using "this") to the listener of the pbody. This makes the Physics module to call the OnCollision method
 	pbody->listener = this;
